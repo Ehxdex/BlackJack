a90cefca3278b0f63b0c6c4edbcb6ea6e0c39a17
rav <reznichenkoav@me.com>
2023-11-27 13:18:37 +0300

    BJ v1 (all_in_one)

--- bj.rb
+++ bj.rb
@@ -1,51 +1,16 @@
 class Player
-	attr_reader :cards
+  attr_reader :cards
 
-	def initialize
-		@cards = []
-    @points = 0
-	end
+  def initialize
+    @cards = []
+  end
 
-	def add_card(card)
+  def add_card(card)
     @cards << card
-	end
+  end
 
-  # to do refactoring!
-  def cards_costs(cards)
-    cards.each do |e|
-      if e.include?("K")
-        @points += 10
-      elsif e.include?("Q")
-        @points += 10
-      elsif e.include?("J")
-        @points += 10
-      elsif e.include?("T")
-        @points += 10
-      elsif e.include?("9")
-        @points += 9
-      elsif e.include?("8")
-        @points += 8
-      elsif e.include?("7")
-        @points += 7
-      elsif e.include?("6")
-        @points += 6
-      elsif e.include?("5")
-        @points += 5
-      elsif e.include?("4")
-        @points += 4
-      elsif e.include?("3")
-        @points += 3
-      elsif e.include?("2")
-        @points += 2
-      elsif e.include?("A")
-        if @points <= 10
-          @points += 11
-        else
-          @points += 1
-        end
-      end
-    end
-    @points
+  def delete_cards
+    @cards = []
   end
 end
 
@@ -59,8 +24,12 @@ class Dealer < Player
     super
 	end
 
-  def get_secret_cards
-    @secret_cards.push(" * " * @cards.size)
+  def gets_secret_cards
+    @secret_cards.push(' * ' * @cards.size)
+  end
+
+  def move
+    # to do
   end
 end
 
@@ -70,6 +39,11 @@ class User < Player
   def initialize
     @money = 0
     super
+  end
+
+  def enter_user_name
+    puts 'Введите Ваше имя:'
+    @name = gets.chomp.to_s
   end
 end
 
@@ -92,6 +66,24 @@ class Deck
     @deck.pop
   end
 
+  def cards_costs(cards)
+    @points = 0
+    cards.each do |e|
+      if e.include?("K") || e.include?("Q") || e.include?("J") || e.include?("T")
+        @points += 10
+      elsif e.include?("A")
+        if @points <= 10
+          @points += 11
+        else
+          @points += 1
+        end
+      else 
+        @points += e.chr.to_i
+      end
+    end
+    @points
+  end
+
   private
 
   def generate_deck
@@ -112,146 +104,162 @@ end
 class Game
   def initialize
     puts 'Welcome Black Jack v1.0'
-    @round = 1
-    @wins = 0
-    @flag = true
-    @money_in_line = 0
-    @points = 0
+    @round = 1 # порядковый номер раунда
+    @money_in_line = 0 # деньги на кону
   end
 
-  def menu
-    puts "Меню игры:"
-    puts '1. Новая игра'
-    puts '2. Показать правила'
-    puts '0. Выйти'
+  # ход пользователя
+  def user_move
+    puts "1. Добавить карту"
+    puts "2. Открыть карты"
+    puts "3. Пас"
     user_move = gets.chomp.to_i
     case user_move
-    when 1 then play_round
-    when 2 then show_rules
-    when 0 
-      puts "exit"
-    else 
-      puts "Такого значения не существует"
+    when 1 
+      @user.add_card(@deck.take_card)
+    when 2 
+      dealer_move
+    when 3 then pass
     end
   end
 
-  def show_rules
-    pust "Правила игры:"
-  end
-
-  def user_choose
-    puts "1. Добавить карту"
-    puts "2. Открыть карты"
-    puts "3. Сделать ставку."
-    puts "4. Пас"
-    user_move = gets.chomp.to_i
-    case user_move
-    when 1 then money_in_game(make_bet)
-    when 2 then show_result
-    when 3 then money_in_game(make_bet)
-    when 4 then pass
+  def dealer_move
+    if @deck.cards_costs(@dealer.cards) < 17
+      @dealer.add_card(@deck.take_card)
     end
   end
 
   def pass
-    # to do
+    puts "Ходит дилер"
+    dealer_move
+    show_result
   end
 
-  def info
-    puts
-    puts "----------------- Инфо -------------------"
-    puts "Раунд №#{@round}. Победы: #{@wins}. Банк #{@dealer.bank} руб."
-    puts "У вас на счету: #{@user.money} руб."
-    puts "На кону: #{@money_in_line} руб."
-  end
-
-  def show_result
-    info
-    puts "----------------- Игра -------------------"
-    puts "Ваши карты: #{@user.cards}"
-    puts "Карты дилера: #{@dealer.cards}"
-    puts "------------------------------------------"
-    puts @user.cards_costs(@user.cards)
-    puts @dealer.cards_costs(@dealer.cards)
-    check_results
-  end
-
+  # сделать рефакторинг
+  # подсчитать результаты раунда
   def check_results
-    if @user.cards_costs(@user.cards) <= 21 || @user.cards_costs(@user.cards) > @dealer.cards_costs(@dealer.cards)
+    if @deck.cards_costs(@dealer.cards) > 21
       puts "Вы выиграли"
-    elsif @user.cards_costs(@user.cards) == @dealer.cards_costs(@dealer.cards)
-      puts "Ничья"
-    else
+      @user.money += @money_in_line
+      @money_in_line = 0
+    elsif @deck.cards_costs(@user.cards) > 21
       puts "Вы проиграли"
+      @dealer.bank += @money_in_line
+      @money_in_line = 0
+    elsif @deck.cards_costs(@user.cards) > @deck.cards_costs(@dealer.cards)
+      puts "Вы выиграли"
+      @user.money += @money_in_line
+      @money_in_line = 0
+    elsif @deck.cards_costs(@user.cards) < @deck.cards_costs(@dealer.cards)
+      puts "Вы проиграли"
+      @dealer.bank += @money_in_line
+      @money_in_line = 0
+    else 
+      puts "Ничья"
+      @user.money += @money_in_line / 2
+      @dealer.bank += @money_in_line / 2
+      @money_in_line = 0
     end
   end
 
-  def make_bet
-    puts 'Ваша ставка?'
-    @bet = gets.chomp_to_i
-  end
-
-  def enter_user_name
-    puts 'Enter name:'
-    @user.name = gets.chomp.to_s
-  end
-
+  # переход денег к пользователю
   def add_user_money(count)
     @dealer.bank -= count
     @user.money += count
   end
 
+  # переход денег в банк
   def add_bank_money(count)
     @user.money -= count
     @dealer.bank += count
   end
 
+  # ввод денег в игру
   def money_in_game(count)
     @user.money -= count
     @dealer.bank -= count
     @money_in_line += (count * 2)
   end
 
+  # сделать рефакторинг следующих 3-х методов
+  # header - общая информация
+  def info
+    puts
+    puts "----------------- Инфо -------------------"
+    puts "Раунд №#{@round}. Банк #{@dealer.bank} руб."
+    puts "#{@user.name} у вас на счету: #{@user.money} руб."
+    puts "На кону: #{@money_in_line} руб."
+  end
+
+  # текущий статус игры
   def game_status
     info
     puts "----------------- Игра -------------------"
     puts "Ваши карты: #{@user.cards}"
-    puts "Карты дилера: #{@dealer.get_secret_cards}"
+    puts "Карты дилера: #{@dealer.gets_secret_cards} #{@dealer.cards}debug_info"
     puts "------------------------------------------"
-    puts @user.cards_costs(@user.cards)
+    puts @deck.cards_costs(@user.cards)
+  end
+
+  # показать результаты игры
+  def show_result
+    info
+    puts "----------------- Игра -------------------"
+    puts "Ваши карты: #{@user.cards}"
+    puts "Карты дилера: #{@dealer.cards}"
+    puts "------------------------------------------"
+    puts @deck.cards_costs(@user.cards)
+    puts @deck.cards_costs(@dealer.cards)
+  end
+
+  def game_init
+     # создаем сущности
+     @user = User.new
+     @dealer = Dealer.new
+     @user.enter_user_name # запрашиваем имя пользователя
+     add_user_money(100) # выдаем деньги пользователю
   end
 
   def play_round
-    @user = User.new
-    @dealer = Dealer.new
+    @user.delete_cards
+    @dealer.delete_cards
     @deck = Deck.new
-
-    enter_user_name
-    add_user_money(100)
-    @deck.cards_shuffle
-    @user.add_card(@deck.take_card)
+    @deck.cards_shuffle # тусуем карты
+    # добавляем карты игроку и дилеру
+    @user.add_card(@deck.take_card) 
     @user.add_card(@deck.take_card)
     @dealer.add_card(@deck.take_card)
     @dealer.add_card(@deck.take_card)
-    @dealer.get_secret_cards
-    money_in_game(20)
-
+    money_in_game(10) # Передаем деньги от игрока и дилера деньги на кон 10 + 10
+    puts
     puts "Ставки сделаны"
-    game_status
+    game_status # выводим статус с игры (текущие очки, карты, раунд)
+    
+    loop do
+      user_move
+      if @user.cards.size <= 3 && @dealer.cards.size <= 3
+        show_result
+        puts "ИТОГИ ИГРЫ"
+        check_results
+      end
 
-    user_choose
-    check_results
+      puts "Хотите сыграть еще раз? 1(да)/2(нет)"
+      user_input = gets.chomp.to_i
+      if user_input == 1
+        @round += 1
+        play_round
+      elsif user_input == 2
+        puts "Конец игры."
+        break
+      end
+    end
   end
 
-  def lets_play
-    # loop do
-      menu
-      play_round
-      user_choose
-      check_results
-    # end
+  def play_game
+    game_init
+    play_round
   end
 end
 
 game = Game.new
-game.play_round
+game.play_game
