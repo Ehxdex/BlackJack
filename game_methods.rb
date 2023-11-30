db3e30c0eaab042647539e4b99fec3d20d68858b
rav <reznichenkoav@me.com>
2023-11-30 12:06:17 +0300

    Add bet method

--- game_methods.rb
+++ game_methods.rb
@@ -7,16 +7,28 @@ class GameMethods
     system 'clear' or system 'cls'
   end
 
+  def make_bet(user, dealer, bet)
+    @money_in_game = bet * 2
+    user.bank -= bet
+    dealer.bank -= bet
+  end
+
   def define_winner(user, dealer)
     if dealer.points > 21 && dealer.points != user.points
+      user.bank += @money_in_game
       "Победил игрок"
     elsif user.points > 21 && dealer.points != user.points
+      dealer.bank += @money_in_game
       "Победил дилер"
     elsif user.points > dealer.points
+      user.bank += @money_in_game
       "Победил игрок"
     elsif user.points < dealer.points
+      dealer.bank += @money_in_game
       "Победил дилер"
     else 
+      user.bank += @money_in_game / 2
+      dealer.bank += @money_in_game / 2
       "Ничья"
     end
   end
@@ -34,16 +46,19 @@ class GameMethods
     dealer.add_card(deck.take_card)
     dealer.add_card(deck.take_card)
     
+    make_bet(user, dealer, 10)
+
     while @rounds < 2
       cls
-      puts "Ход №#{@rounds += 1}"
+
+      puts "Игра №#{@rounds += 1}"
+      puts "Деньги в игре: #{@money_in_game}"
       puts "Деньги игрока: #{user.bank} ||| Деньги дилера: #{dealer.bank}"
       puts "Карты игрока: #{user.show_cards}, очки игрока: #{user.points}"
       puts "Карты дилера: #{dealer.show_cards(secret=true)}"
 
       case user.move
       when 1
-        user.add_card(deck.take_card)
         user.add_card(deck.take_card)
         dealer.move(deck.take_card)
         break
@@ -52,10 +67,11 @@ class GameMethods
       end
     end
 
+    cls
+    puts define_winner(user, dealer)
+
     puts "Деньги игрока: #{user.bank} ||| Деньги дилера: #{dealer.bank}"
     puts "Карты игрока: #{user.show_cards}, очки игрока: #{user.points}"
     puts "Карты дилера: #{dealer.show_cards(secret=false)}, очки дилера: #{dealer.points}"
-    
-    puts define_winner(user, dealer)
   end
 end
